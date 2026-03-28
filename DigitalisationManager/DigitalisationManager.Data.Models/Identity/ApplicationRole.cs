217099c911b2ee2c28f83f4a291bf3f8fe9a5c52
Zarizoto <meslekovpavel@gmail.com>
2026-03-28 10:56:35 +0200

    Add custom application user and role classes

---
+++ DigitalisationManager/DigitalisationManager.Data.Models/Identity/ApplicationRole.cs
@@ -0,0 +1,13 @@
+﻿namespace DigitalisationManager.Data.Models.Identity
+{
+    using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+        [PersonalData]
+        [DataType(DataType.Text)]
+        [MaxLength(100)]
+        public string? Lable { get; set; }
+    }
+}
---
+++ DigitalisationManager/DigitalisationManager.Data.Models/Identity/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿namespace DigitalisationManager.Data.Models.Identity
+{  
+    using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        [PersonalData]
+        [DataType(DataType.Text)]
+        [MaxLength(100)]
+        public string? FirstName { get; set; }
+
+        [PersonalData]
+        [DataType(DataType.Text)]
+        [MaxLength(100)]
+        public string? LastName { get; set; }
+    }
+}
