4cb3e98611971435678d12ca4572cde56fd07cb5
Zarizoto <meslekovpavel@gmail.com>
2026-03-30 16:09:10 +0300

    Simplify global home and remove management navigation

---
+++ DigitalisationManager/DigitalisationManager.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿
+namespace DigitalisationManager.Web.Controllers
+{
+    using DigitalisationManager.Web.ViewModels;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View("~/Views/Shared/AccessDenied");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
