27cd356666c40b78e43b8bb1411011553815993d
Zarizoto <meslekovpavel@gmail.com>
2026-04-01 14:55:02 +0300

    Add user funds and item controllers and views

---
+++ DigitalisationManager/DigitalisationManager.Web/Areas/User/Controllers/FundsController.cs
@@ -0,0 +1,26 @@
+﻿
+namespace DigitalisationManager.Web.Areas.User.Controllers
+{
+    using DigitalisationManager.Services.Core.Contracts;
+    using DigitalisationManager.Web.ViewModels.Funds;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class FundsController : UserBaseController
+    {
+        private readonly IFundService fundService;
+
+        public FundsController(IFundService fundService)
+        {
+            this.fundService = fundService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string? q, int page = 1, int pageSize = 20)
+        {
+            FundQueryViewModel model = await fundService.GetIndexAsync(q, page, pageSize);
+
+            return View(model);
+        }
+    }
+}
---
+++ DigitalisationManager/DigitalisationManager.Web/Areas/User/Controllers/ItemsController.cs
@@ -0,0 +1,43 @@
+﻿namespace DigitalisationManager.Web.Areas.User.Controllers
+{
+    using DigitalisationManager.Services.Core.Contracts;
+    using DigitalisationManager.Web.ViewModels.Item;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ItemsController : UserBaseController
+    {
+        private readonly IItemService itemService;
+        private readonly IDigitalFileService digitalFileService;
+
+        public ItemsController(
+            IItemService itemService,
+            IDigitalFileService digitalFileService)
+        {
+            this.itemService = itemService;
+            this.digitalFileService = digitalFileService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int? fundId, string? q, int page = 1, int pageSize = 20)
+        {
+            ItemQueryViewModel model = await itemService.GetIndexAsync(fundId, q, page, pageSize);
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            ItemDetailsViewModel? model = await itemService.GetDetailsAsync(id);
+            if (model is null)
+            {
+                return NotFound();
+            }
+
+            model.Files = await digitalFileService.ListByItemAsync(id);
+            model.FilesCount = model.Files.Count;
+
+            return View(model);
+        }
+    }
+}
