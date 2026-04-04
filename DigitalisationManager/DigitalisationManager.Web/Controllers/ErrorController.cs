c6bf47820b4266f846b0c7a065bbcbc16a7de56e
Zarizoto <meslekovpavel@gmail.com>
2026-04-04 14:51:46 +0300

    Configure status code re-execution for not found pages and add dedicated error controller actions

---
+++ DigitalisationManager/DigitalisationManager.Web/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿namespace DigitalisationManager.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode:int}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            return statusCode switch
+            {
+                404 => View("NotFound"),
+                _ => View("ServerError")
+            };
+        }
+
+        [Route("Error/500")]
+        public IActionResult ServerError()
+        {
+            return View("ServerError");
+        }
+    }
+}
--- DigitalisationManager/DigitalisationManager.Web/Program.cs
+++ DigitalisationManager/DigitalisationManager.Web/Program.cs
@@ -56,10 +56,11 @@ namespace DigitalisationManager.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Error/500");
                 app.UseHsts();
             }
+
+            app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
             app.UseHttpsRedirection();
             app.UseRouting();
