4b7218bdd9172f6adbb1cfb02a78c5672eeefb4c
Zarizoto <meslekovpavel@gmail.com>
2026-04-05 15:37:46 +0300

    Add batch upload result models for TIFF workflow

---
+++ DigitalisationManager/DigitalisationManager.Web.ViewModels/DigitalFile/BatchDigitalFileUploadResultViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DigitalisationManager.Web.ViewModels.DigitalFile
+{
+    public class BatchDigitalFileUploadResultViewModel
+    {
+        public int TotalCount { get; set; }
+
+        public int SuccessCount { get; set; }
+
+        public int FailedCount { get; set; }
+
+        public List<DigitalFileUploadResultViewModel> Results { get; set; } = new();
+    }
+}
---
+++ DigitalisationManager/DigitalisationManager.Web.ViewModels/DigitalFile/DigitalFileUploadResultViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DigitalisationManager.Web.ViewModels.DigitalFile
+{
+    public class DigitalFileUploadResultViewModel
+    {
+        public string FileName { get; set; } = null!;
+
+        public bool Success { get; set; }
+
+        public string? Error { get; set; }
+    }
+}
