ceeb682fcb6b0665d72c14578a22f1d22d64a654
Zarizoto <meslekovpavel@gmail.com>
2026-04-05 17:20:45 +0300

    Add safe forbidden and error controller responses

--- DigitalisationManager/DigitalisationManager.Web/Controllers/ErrorController.cs
+++ DigitalisationManager/DigitalisationManager.Web/Controllers/ErrorController.cs
@@ -9,9 +9,23 @@
         {
             return statusCode switch
             {
+                403 => View("Forbidden"),
                 404 => View("NotFound"),
+                500 => View("ServerError"),
                 _ => View("ServerError")
             };
+        }
+
+        [Route("Error/403")]
+        public IActionResult Forbidden()
+        {
+            return View("Forbidden");
+        }
+
+        [Route("Error/404")]
+        public IActionResult NotFoundPage()
+        {
+            return View("NotFound");
         }
 
         [Route("Error/500")]
